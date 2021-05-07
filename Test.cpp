7623b68d2cb181c18d2d68349e2ab5bdfdb9da5b
jordantangy@gmail.com <jordantangy@gmail.com>
2021-05-08 01:50:29 +0300

    update

---
+++ Test.cpp
@@ -0,0 +1,55 @@
+#include "doctest.h"
+#include "Board.hpp"
+
+#include "OperationsExpert.hpp"
+#include "Researcher.hpp"
+#include "Scientist.hpp"
+#include "FieldDoctor.hpp"
+#include "GeneSplicer.hpp"
+#include "Dispatcher.hpp"
+#include "Medic.hpp"
+#include "Virologist.hpp"
+
+using namespace pandemic;
+
+
+TEST_CASE("Board operator[] and isclean function"){
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+
+
+}
+
+TEST_CASE("functions"){
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+    CHECK(true == true);
+
+
+}
